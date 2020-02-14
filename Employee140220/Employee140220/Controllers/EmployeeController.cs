f4ff6a4e111a459cfb556bab011d0ac25359e4a3
brintasabit <brintasabit@gmail.com>
2020-02-14 19:38:13 +0600

    Div properties done in dashboard. But view is not clear. 140220/07.37

---
+++ Employee140220/Employee140220/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Employee140220.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Employee()
+        {
+            return View();
+        }
+    }
+}
---
+++ Employee140220/Employee140220/Scripts/Scripts.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function() {
+	$('#lnkdashboard').click(function() {
+		Dashboard();
+    });
+
+});
+
+var Dashboard = function() {
+    $('#content').load('/Employee/Dashboard');
+    //$('#content2').load('/Employee/Dashboard');
+    //$('#content3').load('/Employee/Dashboard');
+}
