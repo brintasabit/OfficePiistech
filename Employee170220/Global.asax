1a54e1b9ca9961354a02f29334e95ba88ac1316f
brintasabit <55041159+brintasabit@users.noreply.github.com>
2020-02-17 20:06:30 +0600

    170220
    Done Partially 170220

---
+++ Employee170220/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Employee170220.MvcApplication" Language="C#" %>
---
+++ Employee170220/Scripts/Scripts.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    $('#addEmployee').click(function () {
+		AddEmployee();
+	});
+	$('#home').click(function () {
+		Home();
+	});
+	$('#lnkdashboard').click(function () {
+		Dashboard();
+    });
+	$('#employee').click(function () {
+		Employee();
+	});
+
+
+});
+
+
+var AddEmployee = function () {
+    alert('fire');
+	$('#main').load('/Employee/AddOrEdit');
+}
+var Home = function () {
+	$('#main').load('/Employee/Dashboard');
+}
+var Dashboard = function () {
+	$('#main').load('/Employee/Dashboard');
+}
+var Employee = function () {
+	$('#main').load('/Employee/Employee');
+}
+
+
