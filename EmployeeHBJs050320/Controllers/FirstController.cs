61b35dfb2fb3a4d8c0ebebfecab443461f76193b
brintasabit <55041159+brintasabit@users.noreply.github.com>
2020-03-05 18:46:27 +0600

    Partial Template
    A partial template added but not useful

---
+++ EmployeeHBJs050320/Controllers/FirstController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeHBJs050320.Models;
+
+namespace EmployeeHBJs050320.Controllers
+{
+    public class FirstController : Controller
+    {
+
+        IEnumerable<EmployeeInfo> GetAllEmployee()
+        {
+            using (DBModel dB = new DBModel())
+            {
+                var employeeList = dB.EmployeeInfoes.ToList<EmployeeInfo>();
+                return employeeList;
+            }
+
+        }
+
+        public ActionResult Hbjs()
+        {
+            return View();
+        }
+        public JsonResult HbJsController()
+        {
+            var employeeInfo6 = GetAllEmployee();
+            
+            var response =
+                new
+                {
+                    EmployeeInfo = employeeInfo6,
+                    
+                };
+
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ EmployeeHBJs050320/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployeeHBJs050320.MvcApplication" Language="C#" %>
