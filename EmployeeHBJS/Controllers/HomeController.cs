170de22d630b0fc7812e941bb745ae4266eebaa9
brintasabit <55041159+brintasabit@users.noreply.github.com>
2020-03-22 23:48:32 +0600

    Paging
    paging done index.cshtml
    partially done in hbjs.cshtml

--- EmployeeHBJS/Controllers/HomeController.cs
+++ EmployeeHBJS/Controllers/HomeController.cs
@@ -4,35 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EmployeeHBJS.Models;
+using PagedList;
+using PagedList.Mvc;
 
 namespace EmployeeHBJS.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(int ?i)
         {
-            return View(GetAllEmployee());
+            return View(GetAllEmployee(i));
         }
 
-        IEnumerable<EmployeeInfo5> GetAllEmployee()
+        IEnumerable<EmployeeInfo5> GetAllEmployee(int ?i)
         {
             using (DBModel dB = new DBModel())
             {
+                
                 var employeeList = dB.EmployeeInfo5.ToList<EmployeeInfo5>();
-                return employeeList;
+                return employeeList.ToPagedList(i ?? 1,10);
             }
 
         }
-
-        public ActionResult EmployeeHBJS()
+        IPagedList GetAllEmployee2(int ?i)
         {
-            return View();
+            using (DBModel dB = new DBModel())
+            {
+                
+                var employeeList = dB.EmployeeInfo5.ToList<EmployeeInfo5>();
+                return employeeList.ToPagedList(i ?? 1,5);
+            }
+
         }
-        public JsonResult HbJs()
+        public ActionResult EmployeeHBJS(int ?pageNo)
         {
-            var employeeInfo5 = GetAllEmployee();
-
-            return Json(new {EmployeeInfo5 = employeeInfo5 });
+            return View(pageNo);
+        }
+        public JsonResult HbJs(int ?pageNo)
+        {
+            var employeeInfo5 = GetAllEmployee(pageNo);
+            var e = new {EmployeeInfo5 = employeeInfo5};
+            
+            return Json(e);
         }
     }
 }
