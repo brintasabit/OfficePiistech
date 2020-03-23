e0ba1686e14cc35cefe40e34825af650b47531d6
brintasabit <55041159+brintasabit@users.noreply.github.com>
2020-03-23 14:41:40 +0600

    paging
    Page number can't pass..AJAX

--- EmployeeHBJS/Controllers/HomeController.cs
+++ EmployeeHBJS/Controllers/HomeController.cs
@@ -11,38 +11,30 @@ namespace EmployeeHBJS.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(int ?i)
+        public ActionResult Index(int ?pageNo)
         {
-            return View(GetAllEmployee(i));
+            return View(GetAllEmployee(pageNo) as IPagedList<EmployeeInfo5>);
         }
 
-        IEnumerable<EmployeeInfo5> GetAllEmployee(int ?i)
+        IEnumerable<EmployeeInfo5> GetAllEmployee(int ?pageNo)
         {
             using (DBModel dB = new DBModel())
             {
                 
                 var employeeList = dB.EmployeeInfo5.ToList<EmployeeInfo5>();
-                return employeeList.ToPagedList(i ?? 1,10);
+                var e = employeeList.ToPagedList(pageNo??1,20);
+                return e;
             }
 
         }
-        IPagedList GetAllEmployee2(int ?i)
+        public ActionResult EmployeeHBJS()
         {
-            using (DBModel dB = new DBModel())
-            {
-                
-                var employeeList = dB.EmployeeInfo5.ToList<EmployeeInfo5>();
-                return employeeList.ToPagedList(i ?? 1,5);
-            }
-
+            return View();
         }
-        public ActionResult EmployeeHBJS(int ?pageNo)
+        public JsonResult HbJs(int? pageNo)
         {
-            return View(pageNo);
-        }
-        public JsonResult HbJs(int ?pageNo)
-        {
-            var employeeInfo5 = GetAllEmployee(pageNo);
+            ;
+            var employeeInfo5 = GetAllEmployee(pageNo??1);
             var e = new {EmployeeInfo5 = employeeInfo5};
             
             return Json(e);
